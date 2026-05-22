c4790b9d1aa4ecc9e94ca9261667c8d6b928dbb4
Muhammad Haseeb Ur rehman <rehmanhaseeb2345@gmail.com>
2026-05-22 12:53:11 +0100

    User & Video Model Added

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { mongo, Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: [true, "Upload Video"],
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    tittle: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    ispublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const video = mongoose.model("Video", videoSchema);
